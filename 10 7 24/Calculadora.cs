c21ca8aa856db1822b63aa18c3826ffc19662872
jcsalamanca-uam <jcsalamanca@uamv.edu.ni>
2024-10-07 09:48:58 -0600

    Add project files.

---
+++ 10 7 24/Calculadora.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_7_24
+{
+    internal class Calculadora
+    {
+        public double Suma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Resta(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiplicacion(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Division(double num1, double num2)
+        {
+            try
+            {
+
+
+                return num1 / num2;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        public double Potencia(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+
+        public double Raiz(double num1, double num2)
+        {
+            return Math.Pow(num1, 1.0 / num2);
+        }
+
+
+
+
+    }
+}
---
+++ 10 7 24/Form1.cs
@@ -0,0 +1,10 @@
+namespace _10_7_24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
